0fdd0dc57dc709c2e0d83cbcd7da18f83ce5ab16
Brian Steffens <briansteffens@gmail.com>
2017-05-23 01:35:17 -0400

    Add list loader

---
+++ src/List.cs
@@ -0,0 +1,65 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace RM.v38
+{
+    public class ListItem
+    {
+        // Descriptive name
+        public string Name { get; set; }
+
+        // Item ID
+        public int ID { get; set; }
+
+        // The RLE file the resource is located in
+        public int File { get; set; }
+
+        // The index of the resource within the RLE file
+        public int Index { get; set; }
+    }
+
+    // Represents a resource list (*.lst) file
+    public class List
+    {
+        public List<ListItem> Items { get; protected set; }
+
+        public List()
+        {
+            Items = new List<ListItem>();
+        }
+
+        public List(string filename)
+            : this()
+        {
+            Load(filename);
+        }
+
+        public void Load(string filename)
+        {
+            var br = new ByteReader(File.ReadAllBytes(filename));
+
+            if (br.STRING() != "RedMoon Lst File" || br.STRING() != "1.0")
+            {
+                throw new InvalidFileException();
+            }
+
+            // TODO: Unknown value
+            br.UINT();
+
+            Items.Clear();
+            uint count = br.UINT();
+
+            for (int i = 0; i < count; i++)
+            {
+                Items.Add(new ListItem()
+                {
+                    Name = br.STRING(),
+                    ID = (int)br.UINT(),
+                    File = (int)br.UINT(),
+                    Index = (int)br.UINT()
+                });
+            }
+        }
+    }
+}
